47b324a1f2b856751f3f5f025e8b7170a8d59384
JohnDev19 <137758073+JohnDev19@users.noreply.github.com>
2024-11-18 18:40:50 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,8 +3,10 @@ document.addEventListener('DOMContentLoaded', () => {
   particlesJS("particles-js", {
     particles: {
       number: {
-        value: 80, density: {
-          enable: true, value_area: 800
+        value: 80, 
+        density: {
+          enable: true, 
+          value_area: 800
         }
       },
       color: {
@@ -14,32 +16,50 @@ document.addEventListener('DOMContentLoaded', () => {
         type: "circle"
       },
       opacity: {
-        value: 0.5, random: false
+        value: 0.5, 
+        random: false
       },
       size: {
-        value: 3, random: true
+        value: 3, 
+        random: true
       },
       line_linked: {
-        enable: true, distance: 150, color: "#00ffff", opacity: 0.4, width: 1
+        enable: true, 
+        distance: 150, 
+        color: "#00ffff", 
+        opacity: 0.4, 
+        width: 1
       },
       move: {
-        enable: true, speed: 2, direction: "none", random: false, straight: false, out_mode: "out", bounce: false
+        enable: true, 
+        speed: 2, 
+        direction: "none", 
+        random: false, 
+        straight: false, 
+        out_mode: "out", 
+        bounce: false
       }
     },
     interactivity: {
       detect_on: "canvas",
       events: {
         onhover: {
-          enable: true, mode: "repulse"
-        }, onclick: {
-          enable: true, mode: "push"
-        }, resize: true
+          enable: true, 
+          mode: "repulse"
+        }, 
+        onclick: {
+          enable: true, 
+          mode: "repulse"
+        }, 
+        resize: true
       },
       modes: {
         repulse: {
-          distance: 100, duration: 0.4
-        }, push: {
-          particles_nb: 4
+          distance: 100, 
+          duration: 0.4
+        },
+        push: {
+          particles_nb: 0 
         }
       }
     },
